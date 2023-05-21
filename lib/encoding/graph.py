90fd9965772254efaa17bb567424b330b274218c
Matthias Hummel <matthiashummel@ymail.com>
2023-05-21 16:59:22 +0200

    perform one hot encoding on atom name features

--- lib/encoding/graph.py
+++ lib/encoding/graph.py
@@ -1,20 +1,43 @@
-
 import clingo
 import networkx as nx
+import numpy as np
+
+from sklearn.preprocessing import OneHotEncoder
 
 
-def generate_dependency_graph(ctl):
-    graph = nx.DiGraph()
-    ctl.ground()
+class DependencyGraph:
 
-    for atom in ctl.symbolic_atoms:
-        if atom.is_fact:
-            # Handle facts
-            graph.add_node(atom.literal)
-        else:
+    def __init__(self):
+        self.graph = nx.DiGraph()
+        self.atom_set = set()
+        self.atom_labels = dict()
+        self.encoder = OneHotEncoder()
+
+    def parse_dependencies(self, ctl):
+        ctl.ground()
+
+        for atom in ctl.symbolic_atoms:
+            name = atom.symbol.name
+            self.atom_set.add(name)
+            self.graph.add_node(atom.literal, label=name)
+
+            if atom.is_fact:
+                continue
+
             # Handle rules
             rule = atom.symbol
             for symbol in rule.arguments:
-                if symbol.type.name == "Number":
-                    graph.add_edge(atom.literal, symbol.number)
-    return graph
+                if symbol.type == clingo.SymbolType.Number:
+                    self.graph.add_edge(atom.literal, symbol.number)
+
+        atom_array = np.array(list(self.atom_set)).reshape(-1, 1)
+        self.encoder.fit(atom_array)
+
+    def encode_atoms(self):
+        labels = [attributes["label"] for _, attributes in self.graph.nodes.data()]
+        label_array = np.array(labels).reshape(-1, 1)
+        features = self.encoder.transform(label_array)
+
+        for i, node_id in enumerate(self.graph.nodes()):
+            node = self.graph.nodes[node_id]
+            node["feature"] = features[i]
--- main.py
+++ main.py
@@ -14,8 +14,9 @@ def run_dependency():
     for asp_file in asp_files:
         ctl.load(str(asp_file))
 
-    dependency_graph = graph.generate_dependency_graph(ctl)
-    print(dependency_graph)
+    dependency_graph = graph.DependencyGraph()
+    dependency_graph.parse_dependencies(ctl)
+    dependency_graph.encode_atoms()
 
 
 if __name__ == "__main__":
