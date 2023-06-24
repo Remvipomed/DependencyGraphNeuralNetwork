ef104c7f5fdebfe4ce1385b2725cc16a806fb71d
Matthias Hummel <matthiashummel@ymail.com>
2023-06-24 16:50:22 +0200

    include optimal solving strategy for measurements

--- lib/heuristic/prediction.py
+++ lib/heuristic/prediction.py
@@ -16,41 +16,59 @@ def evaluate_graph_neural_network():
     heuristic.evaluate(ctl)
 
 
-def run_heuristic():
-    print("heuristic loading")
-    program = project_root.joinpath("demo.asp")
-    ctl = clingo.Control()
-    ctl.load(str(program))
-
-    heuristic = GraphNeuralNetworkHeuristic("demo")
-    heuristic.load()
-    heuristic.predict(ctl)
-
-    print("heuristic solving")
-    ctl.solve()
+def set_optimal_heuristic(ctl_optimal, model):
+    with ctl_optimal.backend() as backend:
+        for atom in model.context.symbolic_atoms:
+            value = 1 if atom.symbol.positive else -1
+            backend.add_heuristic(atom.literal, clingo.HeuristicType.Init, value, 0, [])
 
 
-def run_solver():
-    print("standard grounding")
+def load_program() -> clingo.Control:
     program = project_root.joinpath("demo.asp")
     ctl = clingo.Control()
     ctl.load(str(program))
     ctl.ground()
+    return ctl
+
+
+def run_heuristic():
+    print("heuristic loading")
+    ctl = load_program()
+    heuristic = GraphNeuralNetworkHeuristic("demo")
+    heuristic.load()
+
+    print("GNN heuristic solving")
+    heuristic_start = time.time()
+    heuristic.predict(ctl)
+    ctl.solve()
+    heuristic_end = time.time()
+    print("GNN heuristic solving duration:", heuristic_end - heuristic_start)
+
+
+def run_solver():
+    ctl = load_program()
 
     print("standard solving")
-    ctl.solve()
-
-
-def measure_solving_times():
-    heuristic_start = time.time()
-    run_heuristic()
-    heuristic_end = time.time()
-    print("Heuristic duration:", heuristic_end - heuristic_start)
-
     standard_start = time.time()
-    run_solver()
+    ctl.solve()
     standard_end = time.time()
-    print("Default duration:", standard_end - standard_start)
+    print("solving duration:", standard_end - standard_start)
 
 
+def run_optimal_solving():
+    print("determine optimal heuristic")
+    ctl = load_program()
+    ctl_optimal = load_program()
 
+    ctl.solve(on_model=lambda x: set_optimal_heuristic(ctl_optimal, x))
+
+    optimal_start = time.time()
+    ctl_optimal.solve()
+    optimal_end = time.time()
+    print("Optimal duration:", optimal_end - optimal_start)
+
+
+def run_performance_measurement():
+    run_heuristic()
+    run_solver()
+    run_optimal_solving()
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from lib.heuristic import training, prediction
 
 def perform_heuristic():
     # training.train_graph_neural_network()
-    prediction.measure_solving_times()
+    prediction.run_performance_measurement()
 
 
 if __name__ == "__main__":
